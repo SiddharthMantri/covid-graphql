21312a1e379c2809ae91ab563cd81a778287737a
Siddharth Mantri <siddharthmantri1@gmail.com>
2020-03-16 22:35:24 +0100

    Moved everything to server/

--- server/app.ts
+++ server/app.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
-import { DataLoader } from "../data/Data";
+import { DataLoader } from "./data/Data";
 
 const app = express();
 const port = 8080;
